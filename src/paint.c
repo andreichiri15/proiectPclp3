a847766be3f0c2c3f60f9134a89529c96a0d1c43
Andrei Chirimeaca <andreichiri15@gmail.com>
2023-05-25 17:33:19 +0300

    Added brush feature

--- src/paint.c
+++ src/paint.c
@@ -2,69 +2,21 @@
 
 #define WINDOW_WIDTH 800
 #define WINDOW_HEIGHT 600
-
-void fill(int x, int y, Uint32 targetColor, Uint32 fillColor,
-		  SDL_Surface *surface)
-{
-	Uint32 *pixels = (Uint32 *)surface->pixels;
-	int width = surface->w;
-	int height = surface->h;
-
-	// Check if the target pixel is out of bounds
-	if (x < 0 || x >= width || y < 0 || y >= height) {
-		return;
-	}
-
-	// Check if the target pixel already has the fill color
-	if (pixels[y * width + x] == fillColor) {
-		return;
-	}
-
-	// Perform a flood fill using a stack-based approach
-	Uint32 stackColor = pixels[y * width + x];
-	int stackSize = 0;
-	int stackCapacity = width * height;
-	SDL_Point *stack = (SDL_Point *)malloc(stackCapacity * sizeof(SDL_Point));
-
-	if (stack == NULL) {
-		return;
-	}
-
-	// Push the starting point to the stack
-	stack[stackSize++] = (SDL_Point){x, y};
-
-	while (stackSize > 0) {
-		// Pop a point from the stack
-		SDL_Point current = stack[--stackSize];
-
-		// Check if the current point is within bounds
-		if (current.x >= 0 && current.x < width && current.y >= 0 &&
-			current.y < height) {
-			// Check if the current point has the target color
-			if (pixels[current.y * width + current.x] == targetColor) {
-				// Set the fill color for the current point
-				pixels[current.y * width + current.x] = fillColor;
-
-				// Push adjacent points to the stack
-				stack[stackSize++] =
-					(SDL_Point){current.x - 1, current.y}; // Left
-				stack[stackSize++] =
-					(SDL_Point){current.x + 1, current.y}; // Right
-				stack[stackSize++] =
-					(SDL_Point){current.x, current.y - 1}; // Up
-				stack[stackSize++] =
-					(SDL_Point){current.x, current.y + 1}; // Down
-			}
-		}
-	}
-
-	free(stack);
-}
+#define DEFAULT_BRUSH_SIZE 1
 
 // Function to erase all colors and set the screen back to full white
-void eraseScreen(SDL_Renderer *renderer, int windowWidth, int windowHeight)
+void eraseScreen(SDL_Renderer *renderer)
 {
 	SDL_SetRenderDrawColor(renderer, 255, 255, 255,
+						   255); // Set the render draw color to white
+	SDL_RenderClear(renderer); // Clear the renderer with the current draw color
+	SDL_RenderPresent(renderer); // Update the screen with the cleared renderer
+}
+
+// Function to fill the screen with a certain color
+void fill(SDL_Renderer *renderer, Uint8 red, Uint8 green, Uint8 blue)
+{
+	SDL_SetRenderDrawColor(renderer, red, green, blue,
 						   255); // Set the render draw color to white
 	SDL_RenderClear(renderer); // Clear the renderer with the current draw color
 	SDL_RenderPresent(renderer); // Update the screen with the cleared renderer
@@ -78,6 +30,7 @@ int main(void)
 	int isDrawing = 0;
 	int prevMouseX, prevMouseY;
 	Uint8 red = 0, green = 0, blue = 0;
+	int brushSize = DEFAULT_BRUSH_SIZE; // Variable to store the brush size
 
 	SDL_Init(SDL_INIT_VIDEO);
 
@@ -94,7 +47,7 @@ int main(void)
 		switch (event.type) {
 		case SDL_KEYDOWN:
 			if (event.key.keysym.sym == SDLK_e) {
-				eraseScreen(renderer, WINDOW_WIDTH, WINDOW_HEIGHT);
+				eraseScreen(renderer);
 			} else if (event.key.keysym.sym == SDLK_r) {
 				red = 255;
 				green = 0;
@@ -111,15 +64,25 @@ int main(void)
 				red = 255;
 				green = 255;
 				blue = 0;
+			} else if (event.key.keysym.sym == SDLK_w) {
+				red = 255;
+				green = 255;
+				blue = 255;
+			} else if (event.key.keysym.sym == SDLK_1) {
+				brushSize = 1; // Set brush size to 1
+			} else if (event.key.keysym.sym == SDLK_2) {
+				brushSize = 2; // Set brush size to 2
+			} else if (event.key.keysym.sym == SDLK_3) {
+				brushSize = 3; // Set brush size to 3
+			} else if (event.key.keysym.sym == SDLK_4) {
+				brushSize = 4; // Set brush size to 4
 			}
 			break;
 		case SDL_MOUSEBUTTONDOWN:
 			if (event.button.button == SDL_BUTTON_LEFT) {
 				if (SDL_GetModState() & KMOD_LSHIFT) {
 					// Perform fill at the clicked position
-					fill(event.button.x, event.button.y, 0xFFFFFFFF, 0x000000FF,
-						 SDL_GetWindowSurface(window));
-					SDL_UpdateWindowSurface(window);
+					fill(renderer, red, green, blue);
 				} else {
 					isDrawing = 1;
 					prevMouseX = event.button.x;
@@ -135,10 +98,26 @@ int main(void)
 		case SDL_MOUSEMOTION:
 			if (isDrawing) {
 				SDL_SetRenderDrawColor(renderer, red, green, blue, 255);
-                SDL_RenderDrawLine(renderer, prevMouseX, prevMouseY, event.motion.x, event.motion.y);
-                SDL_RenderPresent(renderer);
-                prevMouseX = event.motion.x;
-                prevMouseY = event.motion.y;
+
+				// Draw lines with the current brush size
+				SDL_RenderDrawLine(renderer, prevMouseX, prevMouseY,
+								   event.motion.x, event.motion.y);
+
+				// Draw additional lines for brush size
+				int brushOffset = brushSize;
+				for (int i = -brushOffset; i <= brushOffset; ++i) {
+					for (int j = -brushOffset; j <= brushOffset; ++j) {
+						if (i != 0 || j != 0) {
+							SDL_RenderDrawLine(
+								renderer, prevMouseX + i, prevMouseY + j,
+								event.motion.x + i, event.motion.y + j);
+						}
+					}
+				}
+
+				SDL_RenderPresent(renderer);
+				prevMouseX = event.motion.x;
+				prevMouseY = event.motion.y;
 			}
 			break;
 		}
