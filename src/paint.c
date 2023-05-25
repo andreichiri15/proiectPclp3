17609d755cd3d59c935721485df27d204798e81e
Andrei Chirimeaca <andreichiri15@gmail.com>
2023-05-25 13:19:07 +0300

    Added fill function

--- src/paint.c
+++ src/paint.c
@@ -1,8 +1,65 @@
 #include <SDL2/SDL.h>
-#include <stdlib.h>
 
 #define WINDOW_WIDTH 800
 #define WINDOW_HEIGHT 600
+
+void fill(int x, int y, Uint32 targetColor, Uint32 fillColor,
+		  SDL_Surface *surface)
+{
+	Uint32 *pixels = (Uint32 *)surface->pixels;
+	int width = surface->w;
+	int height = surface->h;
+
+	// Check if the target pixel is out of bounds
+	if (x < 0 || x >= width || y < 0 || y >= height) {
+		return;
+	}
+
+	// Check if the target pixel already has the fill color
+	if (pixels[y * width + x] == fillColor) {
+		return;
+	}
+
+	// Perform a flood fill using a stack-based approach
+	Uint32 stackColor = pixels[y * width + x];
+	int stackSize = 0;
+	int stackCapacity = width * height;
+	SDL_Point *stack = (SDL_Point *)malloc(stackCapacity * sizeof(SDL_Point));
+
+	if (stack == NULL) {
+		return;
+	}
+
+	// Push the starting point to the stack
+	stack[stackSize++] = (SDL_Point){x, y};
+
+	while (stackSize > 0) {
+		// Pop a point from the stack
+		SDL_Point current = stack[--stackSize];
+
+		// Check if the current point is within bounds
+		if (current.x >= 0 && current.x < width && current.y >= 0 &&
+			current.y < height) {
+			// Check if the current point has the target color
+			if (pixels[current.y * width + current.x] == targetColor) {
+				// Set the fill color for the current point
+				pixels[current.y * width + current.x] = fillColor;
+
+				// Push adjacent points to the stack
+				stack[stackSize++] =
+					(SDL_Point){current.x - 1, current.y}; // Left
+				stack[stackSize++] =
+					(SDL_Point){current.x + 1, current.y}; // Right
+				stack[stackSize++] =
+					(SDL_Point){current.x, current.y - 1}; // Up
+				stack[stackSize++] =
+					(SDL_Point){current.x, current.y + 1}; // Down
+			}
+		}
+	}
+
+	free(stack);
+}
 
 int main(void)
 {
@@ -26,7 +83,14 @@ int main(void)
 		switch (event.type) {
 		case SDL_MOUSEBUTTONDOWN:
 			if (event.button.button == SDL_BUTTON_LEFT) {
-				isDrawing = 1;
+				if (SDL_GetModState() & KMOD_LSHIFT) {
+					// Perform fill at the clicked position
+					fill(event.button.x, event.button.y, 0xFFFFFFFF, 0x000000FF,
+						 SDL_GetWindowSurface(window));
+					SDL_UpdateWindowSurface(window);
+				} else {
+					isDrawing = 1;
+				}
 			}
 			break;
 		case SDL_MOUSEBUTTONUP:
@@ -40,7 +104,6 @@ int main(void)
 				SDL_RenderDrawPoint(renderer, event.motion.x, event.motion.y);
 				SDL_RenderPresent(renderer);
 			}
-            
 			break;
 		}
 	}
