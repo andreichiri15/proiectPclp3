7ea15e625716d47d67903c412e4971ed12b485da
Andrei Chirimeaca <andreichiri15@gmail.com>
2023-05-25 18:31:08 +0300

    Added drawing circle

--- src/paint.c
+++ src/paint.c
@@ -1,8 +1,12 @@
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
 
 #define WINDOW_WIDTH 800
 #define WINDOW_HEIGHT 600
 #define DEFAULT_BRUSH_SIZE 1
+
+SDL_Window *window = NULL;
+SDL_Renderer *renderer = NULL;
 
 // Function to erase all colors and set the screen back to full white
 void eraseScreen(SDL_Renderer *renderer)
@@ -22,10 +26,41 @@ void fill(SDL_Renderer *renderer, Uint8 red, Uint8 green, Uint8 blue)
 	SDL_RenderPresent(renderer); // Update the screen with the cleared renderer
 }
 
+void drawCircle(SDL_Renderer *renderer, int centerX, int centerY, int radius,
+				Uint8 red, Uint8 green, Uint8 blue)
+{
+	SDL_SetRenderDrawColor(renderer, red, green, blue, 255);
+
+	int x = 0;
+	int y = radius;
+	int decision = 3 - 2 * radius;
+
+	while (x <= y) {
+		SDL_RenderDrawPoint(renderer, centerX + x, centerY + y);
+		SDL_RenderDrawPoint(renderer, centerX - x, centerY + y);
+		SDL_RenderDrawPoint(renderer, centerX + x, centerY - y);
+		SDL_RenderDrawPoint(renderer, centerX - x, centerY - y);
+		SDL_RenderDrawPoint(renderer, centerX + y, centerY + x);
+		SDL_RenderDrawPoint(renderer, centerX - y, centerY + x);
+		SDL_RenderDrawPoint(renderer, centerX + y, centerY - x);
+		SDL_RenderDrawPoint(renderer, centerX - y, centerY - x);
+
+		x++;
+
+		if (decision < 0) {
+			decision += 4 * x + 6;
+		} else {
+			decision += 4 * (x - y) + 10;
+			y--;
+		}
+	}
+
+	SDL_SetRenderDrawColor(renderer, 0, 0, 0,
+						   255); // Reset the renderer color to black
+}
+
 int main(void)
 {
-	SDL_Window *window = NULL;
-	SDL_Renderer *renderer = NULL;
 	SDL_Event event;
 	int isDrawing = 0;
 	int prevMouseX, prevMouseY;
@@ -76,6 +111,14 @@ int main(void)
 				brushSize = 3; // Set brush size to 3
 			} else if (event.key.keysym.sym == SDLK_4) {
 				brushSize = 4; // Set brush size to 4
+			} else if (event.key.keysym.sym == SDLK_c) {
+				// Get the mouse position
+				int mouseX, mouseY;
+				SDL_GetMouseState(&mouseX, &mouseY);
+
+				// Draw a circle at the mouse position
+				drawCircle(renderer, mouseX, mouseY, 50, red, green, blue);
+				SDL_RenderPresent(renderer);
 			}
 			break;
 		case SDL_MOUSEBUTTONDOWN:
